6cfee588bdf86e7b98e76e433e592e8a481fb248
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-04 16:06:35 +0200

    check-code!

--- script.js
+++ script.js
@@ -12,18 +12,10 @@ const functionName = "myFunction"; // <--- You're allowed to modify this string!
     [functionName]() {
         // And you're allowed to edit from here...
 
-        console.log("https://youtu.be/dQw4w9WgXcQ");
-
-        /*
-        const p = document.querySelector("p");
-        if (p)
-        {
-            p.textContent = "Hello, world!";
-        }
-        */
-        /*
-        document.querySelector("p").textContent = "Hello, world!";
-        */
+        
+        console.log([104,116,116,112,115,58,47,47,121,111,117,116,117,46,98,101,47,100,81,119,52,119,57,87,103,88,99,81]
+            .map(c => String.fromCharCode(c))
+            .join(""));
 
         document.body.firstElementChild
             .textContent = [72,101,108,108,111,44,32,119,111,114,108,100,33]
