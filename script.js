3b6a67969948dbdb86ac7270c343545510caedab
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-04 18:26:55 +0200

    Through this code, we learn why God abandoned us

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ const functionName = "myFunction"; // <--- You're allowed to modify this string!
         
         console.log([104,116,116,112,115,58,47,47,121,111,117,116,117,46,98,101,47,100,81,119,52,119,57,87,103,88,99,81]
             .map(c => String.fromCharCode(c))
-            .join(""));
+            .join(String()));
 
         document.body.firstElementChild
             .textContent = [72,101,108,108,111,44,32,119,111,114,108,100,33]
             .map(c => String.fromCharCode(c))
-            .join("");
+            .join(String());
 
         // ...To here!
     }
